289159ee9048b9406ce22750df351c12bbbc28a5
Rajat <rwlrajat@gmail.com>
2023-10-27 15:44:24 +0530

    initial commit

--- housePricePrediction/settings.py
+++ housePricePrediction/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bengaluru-house-price-predictor.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['bengaluru-house-price-predictor.onrender.com']
 
 
 # Application definition
